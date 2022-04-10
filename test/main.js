f5a60b160055183f466a45211b6ad0ff4fa03cc3
james <jamesrobertcarthew@gmail.com>
2022-04-10 18:40:14 -0400

    Update main.js

--- test/main.js
+++ test/main.js
@@ -1,8 +1,35 @@
 // graphics
 var view, border;
+var centerX, centerY, titleY, buttonY;
 // strings
 var titleStringArr = [];
 var titleString = '';
+var buttons = [];
+var buttonLabels = ['music', 'blog', 'code/art'];
+var titleWidth;
+
+
+class myButton {
+  constructor(label, x, y) {
+    this.label = '|';
+    for (let i = 0; i < label.length + 3; i++) {
+      this.label += '-';
+    }
+    this.label += '|\n| ' + label + '  |\n|';
+    for (let i = 0; i < label.length + 3; i++) {
+      this.label += '-';
+    }
+    this.label += '|';
+    // this.label = label;
+    this.x = x;
+    this.y = y;
+    this.width;
+    this.x_min;
+    this.x_max;
+    this.y_min;
+    this.y_max;
+  }
+}
 
 function setupScreen() {
   // draw-onables
@@ -12,10 +39,25 @@ function setupScreen() {
   let y = (windowHeight - 2*border);
   view = createGraphics(x, y);
   // text setup
+  centerX = int(width/2);
+  centerY = int(height/2);
+  titleY = int(height/3);
+  buttonY = height - titleY;
   let tSize = int(0.02 * x);
   textSize(tSize);
   textFont('Courier New');
-
+  titleWidth = textWidth(titleStringArr[1]);
+  buttons = [];
+  // button setup
+  let buttonX = int(centerX - (titleWidth/2));
+  console.log(buttonX);
+  let xInt = int(titleWidth/(buttonLabels.length - 1));
+  console.log(xInt);
+  for (let i = 0; i < buttonLabels.length; i++){
+    buttons.push(new myButton(buttonLabels[i], buttonX, buttonY));
+    // console.log(buttons[i].label, buttons[i].x);
+    buttonX += xInt;
+  }
   // prettify
   background(0);
   view.background(255);
@@ -46,7 +88,14 @@ function setup() {
 }
 
 function draw(){
+
   textAlign(CENTER, CENTER);
   image(view, border, border);
-  text(titleString, int(width/2), int(height/2))
+  text(titleString, centerX, titleY)
+  for (let i = 0; i < buttons.length; i++){
+    text(buttons[i].label, buttons[i].x, buttons[i].y);
+  }
+  // stroke(0);
+  // line(width/2, 0, width/2, height);
+  // noStroke();
 }

de3a0170a04f0f77b2636c6fa6f7aab0335094bd
james <jamesrobertcarthew@gmail.com>
2022-04-11 13:37:02 -0400

    Update main.js

--- test/main.js
+++ test/main.js
@@ -160,8 +160,8 @@ function setupScreen() {
   textFont('Courier New');
   titleWidth = textWidth(titleStringArr[1]);
   buttons = [];
-  titleDiv.style('font-size', tSize + 'px');
-  titleDiv.style('font-size', tSize + 'px');
+  titleDiv.style('font-size', textSize() + 'px');
+  titleDiv.style('font-size', textSize() + 'px');
   titleDiv.position(centerX - int(titleWidth/2) + textWidth(titleStringArr[1][3]), titleY - (titleHeight));
   // button setup
   let buttonX = int(centerX - (titleWidth/2));
@@ -260,7 +260,7 @@ function draw(){
   view.noFill();
   view.rect(0, 0, gradient.width, gradient.height);
   image(view, border, border);
-  fill(255, 0, 0);
+  fill(0, 255, 0);
   text(titleString, centerX, titleY);
   noStroke();
   for (let i = 0; i < buttons.length; i++){

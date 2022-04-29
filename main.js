1923331d06d3aac2e3c0ab4f2462d4496e4aea6c
james <jamesrobertcarthew@gmail.com>
2022-04-29 17:41:50 -0400

    Update main.js

--- main.js
+++ main.js
@@ -293,7 +293,7 @@ function checkRightBrain(myTags){
 }
 
 function setupPostDiv() {
-  let divString = '<table width="100%"><tr><td><a href="javascript:void(0)" onclick="filterPosts(0)">All</a></td><td><a href="javascript:void(0)" onclick="filterPosts(1)">Left Brain</a></td><td> <a href="javascript:void(0)" onclick="filterPosts(2)">Right Brain</a></td></tr></table><br>';
+  let divString = '<table width="100%"><tr><td><a href="javascript:void(0)" onclick="filterPosts(0)">All</a></td><td><a href="javascript:void(0)" onclick="filterPosts(1)">Techy</a></td><td> <a href="javascript:void(0)" onclick="filterPosts(2)">Arty</a></td></tr></table><br>';
   for (let i = 0; i < posts.length; i++) {
     if (postFilter == 1) {
       if (checkLeftBrain(posts[i]['tags'])) {
